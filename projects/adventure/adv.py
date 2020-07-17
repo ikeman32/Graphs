6c6d369522f852433d1cd8d70c0965ff2b76d456
David Isakson <ikeman32@yahoo.com>
2020-07-17 05:06:43 -0700

    Adventure Not Working

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -5,16 +5,29 @@ from world import World
 import random
 from ast import literal_eval
 
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
 # Load world
 world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -29,8 +42,165 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+#------------------------------------------------------------------------------------------------------------------------------
+# player.current_room.id
+exits = player.current_room.get_exits()
+# player.travel(direction)
+# print(f' {player.travel("n")}, {player.current_room.id}, {player.current_room.get_exits()} ')
+# print(f' {player.travel("n")}, {player.current_room.id}, {player.current_room.get_exits()} ')
+# print(f' {player.travel("n")}, {player.current_room.id}, {exits} ')
 
+print(f'room_graph: {len(room_graph)} ')
+def get_id():
+    id = player.current_room.id
+    return id
 
+def get_exits():
+    exits = player.current_room.get_exits()
+    return exits
+
+room_map = {}
+room_map[get_id()] = {d: '?' for d in get_exits()}
+
+def get_neighbors(direction):
+    
+    current_room = get_id()
+    # print(direction)
+    if direction == 'n': 
+        player.travel("n")
+        room_map[current_room].update({'n': get_id()})
+        room_map[get_id()] = {d: '?' for d in get_exits()}
+        room_map[get_id()].update({'s': current_room})
+    if direction == 's':
+        player.travel("s")
+        room_map[current_room].update({'s': get_id()})
+        room_map[get_id()] = {d: '?' for d in get_exits()}
+        room_map[get_id()].update({'n': current_room})
+    if direction == 'w':
+        player.travel("w")
+        room_map[current_room].update({'w': get_id()})
+        room_map[get_id()] = {d: '?' for d in get_exits()}
+        room_map[get_id()].update({'e': current_room})   
+    if direction == 'e':
+        player.travel("e")
+        room_map[current_room].update({'e': get_id()})
+        room_map[get_id()] = {d: '?' for d in get_exits()}
+        room_map[get_id()].update({'w': current_room})
+    
+    return get_id()
+
+def visit_all_rooms():
+    s = Stack()   
+    s.push(get_id())
+    rooms_visited = set()
+    current_room = ''
+    next_room = ''
+
+    check_point = set()
+    check_point.add(get_id())
+    to_check_point = False
+    check_point_path = []
+    
+
+    while len(rooms_visited) < len(room_graph) and s.size() != 0:
+        current_room = s.pop()
+        # print(check_point)
+        # if current_room != None:
+        if current_room not in rooms_visited:
+            rooms_visited.add(current_room)
+
+        
+        if room_map[current_room].get('n')  == '?':
+            s.push(get_neighbors('n'))
+        if room_map[current_room].get('s')  != '?':
+            s.push(get_neighbors('s'))
+            
+                
+       
+
+        
+        # if '?' in room_map[current_room].values():
+        # if '?' in room_map[current_room].get('n'):
+        #     next_room = get_neighbors('n')
+        #     check_point_path.append('s')
+        #     # prev_room = get_id()
+        #     s.push(next_room)
+        #     continue
+        # if '?' in room_map[current_room].get('s'):
+        #     next_room = get_neighbors('s')
+        #     check_point_path.append('n')
+        #     # prev_room = get_id()
+        #     s.push(next_room)
+        #     continue
+        # if '?' in room_map[current_room].get('w'):
+        #     next_room = get_neighbors('w')
+        #     check_point_path.append('e')
+        #     # prev_room = get_id()
+        #     s.push(next_room)
+        #     continue
+        # if '?' in room_map[current_room].get('e'):
+        #     next_room = get_neighbors('e')
+        #     check_point_path.append('w')
+        #     # prev_room = get_id()
+        #     s.push(next_room)
+        #     continue
+
+        # if not to_check_point:
+        #     if '?' in room_map[current_room].values():
+        #         if '?' in room_map[current_room].get('n'):
+        #             next_room = get_neighbors('n')
+        #             check_point_path.append('s')
+        #             # prev_room = get_id()
+        #             s.push(next_room)
+        #             continue
+        #         if '?' in room_map[current_room].get('s'):
+        #             next_room = get_neighbors('s')
+        #             check_point_path.append('n')
+        #             # prev_room = get_id()
+        #             s.push(next_room)
+        #             continue
+        #         if '?' in room_map[current_room].get('w'):
+        #             next_room = get_neighbors('w')
+        #             check_point_path.append('e')
+        #             # prev_room = get_id()
+        #             s.push(next_room)
+        #             continue
+        #         if '?' in room_map[current_room].get('e'):
+        #             next_room = get_neighbors('e')
+        #             check_point_path.append('w')
+        #             # prev_room = get_id()
+        #             s.push(next_room)
+        #             continue
+        #         else:
+        #             to_check_point = True
+        # elif to_check_point:
+                
+        #     if current_room in check_point:
+        #         if '?' in room_map[current_room].get('w'):
+        #             next_room = get_neighbors('w')
+        #             check_point_path.append('e')
+        #             # prev_room = get_id()
+        #             s.push(next_room)
+        #             continue
+        #         if '?' in room_map[current_room].get('e'):
+        #             next_room = get_neighbors('e')
+        #             check_point_path.append('w')
+        #             # prev_room = get_id()
+        #             s.push(next_room)
+        #             continue
+        #         else:
+        #             to_check_point = False
+        #     next_room = get_neighbors(check_point_path[-1])
+        #     s.push(next_room)
+        #     check_point_path.pop()
+        #     continue
+
+        # s.push(next_room)
+        print('map:', room_map)
+        print('rooms:',rooms_visited)
+        
+visit_all_rooms()
+#------------------------------------------------------------------------------------------------------------------------------
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
@@ -51,12 +221,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
